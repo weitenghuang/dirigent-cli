b70790024b83c58ef18469505a3b8d004af39291
weitenghuang <weiteng.huang@concur.com>
2016-07-22 23:31:08 -0400

    default build tag
    use “latest” as default local image build tag

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+version="latest"
+if [ ! -z "$1" ]; then
+  version=$1
+fi
+
 docker build -t dirigent-cli-install .
 docker run --name dirigent-cli dirigent-cli-install go build -o /go/bin/dirigent github.com/weitenghuang/dirigent-cli
-docker commit --change 'CMD ["dirigent"]' dirigent-cli dirigent-cli:$1
+docker commit --change 'CMD ["dirigent"]' dirigent-cli dirigent-cli:$version
 docker rm dirigent-cli
